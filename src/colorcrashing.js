1f83283f83d637ae5c7f087127e078825663ee60
Madeleine Daly <mdaly@dailybreak.com>
2018-01-06 14:43:30 -0500

    initial commit; port colorcrashing script

---
+++ src/colorcrashing.js
@@ -0,0 +1,26 @@
+import AviGlitch from 'aviglitch';
+import times from 'lodash/times';
+import random from 'lodash/random';
+import fs from 'fs';
+import path from 'path';
+
+const [ input, output ] = process.argv.slice(2);
+const a = AviGlitch.open(path.resolve(process.cwd(), input));
+
+let deltas = [];
+a.frames.each_with_index((f, i) => {
+  if (f.is_deltaframe) deltas.push(i);
+});
+
+const q = a.frames.slice(0, 6);
+
+times(100, () => {
+  const i = deltas[random(deltas.length)];
+  const x = a.frames.slice(i, i+1);
+  q.concat(x.mul(random(50)));
+});
+
+const dest = path.resolve(process.cwd(), output);
+AviGlitch.open(q).output(dest, true, () => {
+  process.exit(0);
+});
