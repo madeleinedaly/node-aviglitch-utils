ac9c011e67fc7fbb8ec9f2d549c81bece7b9241e
Madeleine Daly <mdaly@dailybreak.com>
2018-01-14 21:18:41 -0500

    add oilpaint script

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # AviGlitch Utils
 
-Node.js port of [aviglitch-utils](https://github.com/ucnv/aviglitch-utils), plus some
+Node.js port of [aviglitch-utils](https://github.com/ucnv/aviglitch-utils), plus [some](https://gist.github.com/ucnv/586209)
 [related](https://gist.github.com/ucnv/466869) [gists](https://gist.github.com/ucnv/772097).
 
 ## Installation
@@ -13,6 +13,7 @@ npm i -g aviglitch-utils
 
 ```sh
 colorcrash --help
+oilpaint --help
 plated-datamosh --help
 squeeze-frame --help
 ```
---
+++ bin/oilpaint.js
@@ -0,0 +1,51 @@
+#!/usr/bin/env node
+'use strict';
+const AviGlitch = require('aviglitch');
+const times = require('lodash/times');
+const meow = require('meow');
+const path = require('path');
+const fs = require('fs');
+const { runcmd } = require('./helper');
+
+const options = {
+  description: false,
+  help: `
+Usage:
+  __FILE__ <infile> [-o <outfile> -u <unit> -s <step>]
+Options:
+  -o --output <outfile>  Set output file name [default: out.avi].
+  -u --unit <unit>       Set unit size [default: 4].
+  -s --step <step>       Set step size [default: 1].
+  -h --help              Show this screen.
+`.replace(/__FILE__/g, path.basename(process.argv[1], '.js')),
+  flags: {
+    output: {type: 'string', alias: 'o', default: 'out.avi'},
+    unit: {type: 'string', alias: 'u', default: '3'},
+    step: {type: 'string', alias: 's', default: '1'}
+  }
+};
+
+(async cli => {
+  if (cli.flags.h) cli.showHelp(0);
+  const [ unit, step ] = ['u', 's'].map(flag => parseInt(cli.flags[flag], 10));
+  const tmp = `${Date.now()}.avi`;
+  await runcmd('ffmpeg', ['-i', cli.input.shift(), '-an', '-vcodec', 'copy', '-r', '30', tmp]);
+  const g = AviGlitch.open(tmp);
+  g.glitch('keyframe', x => null);
+
+  let q = null;
+  const size = g.frames.size();
+  times(Math.ceil(size / step), i => {
+    const pos = i * step;
+    const f = g.frames.slice(pos, pos + unit);
+    if (q === null) {
+      q = f;
+    } else {
+      q.concat(f);
+    }
+    console.log(`>> ${String(pos).padStart(4, '0')}/${size}`);
+  });
+
+  AviGlitch.open(q).output(cli.flags.o, true, () => process.exit());
+  fs.unlink(tmp);
+})(meow(options));
